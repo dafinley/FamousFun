029f5773647ad279589c6f36e534cc3c462eefca
root <root@do.socialchomp.1>
2015-11-01 19:49:29 -0500

    empty

--- node_modules/famous/core/test/pathStore/PathStore.api.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = [
-    'insert',
-    'remove',
-    'get',
-    'getItems',
-    'getPaths'
-];
-
--- node_modules/famous/core/test/transformSystem/TransformSystem.api.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-    'registerTransformAtPath',
-    'deregisterTransformAtPath',
-    'makeBreakPointAt',
-    'get',
-    'onUpdate'
-];
-
